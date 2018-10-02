b39363b42fccdc44f8a02d1fa05f8e8b0dfa0e01
Andre Rocha <andrefmrocha@live.com.pt>
2018-10-02 22:03:26 +0100

    Added 2nd Class and 1st exercise

---
+++ Aula_2/src/Veiculo.cpp
@@ -0,0 +1,25 @@
+#include "Veiculo.h"
+#include <iostream>
+
+using namespace std;
+
+Veiculo::Veiculo(string mc, int m, int a): marca(mc), ano(a), mes(m){}
+
+string Veiculo::getMarca() const
+{
+	return marca;
+}
+
+
+Motorizado::Motorizado(string mc, int m, int a, string c, int cil): Veiculo(mc,m,a),combustivel(c), cilindrada(cil){}
+
+string Motorizado::getCombustivel() const
+{
+	return combustivel;
+}
+
+Automovel::Automovel(string mc, int m, int a,string c, int cil): Motorizado(mc,m,a,c,cil){}
+
+Camiao::Camiao(string mc, int m, int a, string c, int cil, int cm): Motorizado(mc,m,a,c,cil), carga_maxima(cm){}
+
+Bicicleta::Bicicleta(string mc, int m, int a,string t): Veiculo(mc,m,a), tipo(t){}
