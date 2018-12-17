46e2f58e0165423c808421dd2a3c0be94938f0fe
andrefmrocha <andrefmrocha@live.com.pt>
2018-12-17 17:21:09 +0000

    Added files from next test

---
+++ Test4_2015_2016/src/FilmTime.cpp
@@ -0,0 +1,39 @@
+/*
+ * FilmTime.cpp
+ */
+#include "FilmTime.h"
+
+
+FilmTime::FilmTime(unsigned h, Film *f, unsigned id): hour(h), film(f), roomID(id)
+{}
+
+FilmTime::~FilmTime() { }
+
+unsigned FilmTime::getHour() const
+{ return hour; }
+
+unsigned FilmTime::getRoomID() const
+{ return roomID; }
+
+Film* FilmTime::getFilm() const
+{ return film; }
+
+void FilmTime::setFilm(Film* f)
+{ film = f; }
+
+bool FilmTime::operator == (const FilmTime &ft) const
+{ return (film==ft.film && hour==ft.hour && roomID==ft.roomID); }
+
+
+// TODO
+
+bool FilmTime::operator<(const FilmTime & ft1) const
+{
+	if(this->getHour() == ft1.getHour())
+		return this->getRoomID() < ft1.getRoomID();
+	return this->getHour() < ft1.getHour();
+}
+
+
+
+
